4f92ea49a295024e263e145c855c0b4cacd3c3de
nishachandru2000 <nishaa03.vs1999@gmail.com>
2025-12-03 19:19:53 +0530

    first commit -4

--- src/Components/Header/Index.jsx
+++ src/Components/Header/Index.jsx
@@ -3,8 +3,8 @@ import { Link } from "react-router-dom"
 function Header(){
     return <header>
         <Link to={"/"}>Home</Link>
-        <Link to={"/"}>About</Link>
-        <Link to={"/"}>Contact</Link>
+        <Link to={"/About"}>About</Link>
+        <Link to={"/Contact"}>Contact</Link>
         <h1>This is a Paragraph</h1>
         </header>
        }
